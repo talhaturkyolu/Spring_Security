b7c296c7e6e56783665fb882b29b2baa3180c991
talhaturkyolu <turkyolutalha@gmail.com>
2021-03-08 20:09:52 -0600

    Unit Testing MockMVC

---
+++ Spring-13-UnitTesting-MockMvc/src/main/java/com/cybertek/entity/Student.java
@@ -0,0 +1,33 @@
+package com.cybertek.entity;
+
+import com.sun.xml.bind.v2.model.core.ID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
